3fd06bb923f9caeb41be9f2ae80c1d399e98b13c
sulkhan <sulkhangalangsakti@gmail.com>
2023-08-31 20:08:51 +0700

    initial clone

--- src/components/DropdownUser.tsx
+++ src/components/DropdownUser.tsx
@@ -45,8 +45,7 @@ const DropdownUser = () => {
       >
         <span className="hidden text-right lg:block">
           <span className="block text-sm font-medium text-black dark:text-white">
-            Thomas Anree
-          </span>
+Rizki Programmer          </span>
           <span className="block text-xs">UX Designer</span>
         </span>
 
