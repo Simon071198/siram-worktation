0460928714d1cf7550507ff31a48682d9ff8b1de
sulkhan <sulkhangalangsakti@gmail.com>
2023-09-04 16:55:55 +0700

    user list page

--- src/pages/User/UserList.tsx
+++ src/pages/User/UserList.tsx
@@ -21,57 +21,44 @@ const CameraList = () => {
       </h4>
 
       <div className="flex flex-col">
-        <div className="grid grid-cols-8 rounded-sm bg-gray-2 dark:bg-meta-4 sm:grid-cols-8">
+        <div className="grid grid-cols-4 rounded-sm bg-gray-2 dark:bg-meta-4 sm:grid-cols-4">
           <div className="p-2.5 xl:p-5">
             <h5 className="text-sm font-medium uppercase xsm:text-base">
               Nama Pengguna
             </h5>
           </div>
-          {/* <div className="p-2.5 text-center xl:p-5 hidden xl:block">
-            <h5 className="text-sm font-medium uppercase xsm:text-base ">
-              ID Kamera
-            </h5>
-          </div> */}
+      
           <div className="p-2.5 text-center xl:p-5">
             <h5 className="text-sm font-medium uppercase xsm:text-base">
-ID Pengguna            </h5>
+              ID Pengguna{' '}
+            </h5>
+          </div>
+          <div className="p-2.5 text-center xl:p-5">
+            <h5 className="text-sm font-medium uppercase xsm:text-base">
+Tanggal Dibuat            </h5>
           </div>
           <div className="hidden p-2.5 text-center sm:block xl:p-5">
             <h5 className="text-sm font-medium uppercase xsm:text-base">
-Aksi            </h5>
+              Aksi{' '}
+            </h5>
           </div>
-        
         </div>
 
         {data.map((item) => {
           return (
-            <div className="grid grid-cols-8 border-b border-stroke dark:border-strokedark sm:grid-cols-8">
-              <div className="flex items-center gap-3 p-2.5 xl:p-5">
+            <div className="grid grid-cols-4 rounded-sm bg-gray-2 dark:bg-meta-4 sm:grid-cols-4">
+            <div className="flex items-center gap-3 p-2.5 xl:p-5">
                 <p className="hidden text-black dark:text-white sm:block">
-                  {item.deviceCode}
+                  {item.username}
                 </p>
               </div>
 
-              {/* <div className="flex items-center justify-center p-2.5 xl:p-5 hidden xl:block">
-                <p className="text-black dark:text-white text-xs">{item.deviceId}</p>
-              </div> */}
-
               <div className="flex items-center justify-center p-2.5 xl:p-5">
-                <p className="text-meta-3">{item.IpAddress}</p>
+                <p className="text-meta-3">{item.userId}</p>
               </div>
 
               <div className="hidden items-center justify-center p-2.5 sm:flex xl:p-5">
-                <p className="text-black dark:text-white">{item.status}</p>
-              </div>
-
-              <div className="hidden items-center justify-center p-2.5 sm:flex xl:p-5">
-                <p className="text-meta-5"> {item.dmName}</p>
-              </div>
-              <div className="hidden items-center justify-center p-2.5 sm:flex xl:p-5">
-                <p className="text-meta-5"> {item.provider}</p>
-              </div>
-              <div className="hidden items-center justify-center p-2.5 sm:flex xl:p-5">
-                <p className="text-meta-5"> {item.location}</p>
+                <p className="text-black dark:text-white">{item.createstamp}</p>
               </div>
             </div>
           );
