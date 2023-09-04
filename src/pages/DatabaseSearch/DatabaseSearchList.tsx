661e686abdc40ce780809a3f6256fd7ba27aae0a
sulkhan <sulkhangalangsakti@gmail.com>
2023-09-04 17:42:08 +0700

    search inmate by name

--- src/pages/DatabaseSearch/DatabaseSearchList.tsx
+++ src/pages/DatabaseSearch/DatabaseSearchList.tsx
@@ -25,7 +25,7 @@ const DatabaseSearchList = () => {
           </div>{' '}
         </NavLink>
 
-        <NavLink to="/database-search">
+        <NavLink to="/db-inmate-search-by-name">
           <div className="rounded-sm border border-stroke bg-white py-6 px-7.5 shadow-default dark:border-strokedark dark:bg-boxdark">
             <div className="flex h-11.5 w-11.5 items-center justify-center rounded-full bg-meta-2 dark:bg-meta-4">
             <svg fill="none" 
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -23,12 +23,18 @@ const DeviceGatewayList = lazy(() => import('../pages/Device/GatewayList'));
 const UserList = lazy(() => import('../pages/User/UserList'));
 const LocationList = lazy(() => import('../pages/Location/LocationList'));
 const DatabaseSearchList = lazy(() => import('../pages/DatabaseSearch/DatabaseSearchList'));
+const InmateDatabaseSearchByName = lazy(() => import('../pages/DatabaseSearch/InmateDatabaseSearchByName'));
 
 const coreRoutes = [
   {
     path: '/calendar',
     title: 'Calender',
     component: Calendar,
+  },
+  {
+    path: '/db-inmate-search-by-name',
+    title: 'InmateDatabaseSearchByName',
+    component: InmateDatabaseSearchByName,
   },
   {
     path: '/db-search-list',
