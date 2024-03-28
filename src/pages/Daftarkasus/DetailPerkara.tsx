5d266a468554da7b0db8080fa57c9a8c4dda93a4
Simon Tampubolon <smntampubolon18@gmail.com>
2024-03-28 18:47:22 +0700

    Add page Data Umum in detail Daftar Kasus

--- src/pages/Daftarkasus/DetailPerkara.tsx
+++ src/pages/Daftarkasus/DetailPerkara.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import DataUmum from '../../components/DataUmum';
 
 const DetailPerkara = () => {
   const [tapIndex, setTapIndex] = useState(0);
@@ -21,7 +22,11 @@ const DetailPerkara = () => {
           );
         })}
       </div>
-      {tapIndex == 0 && <div>dataumum</div>},
+      {tapIndex == 0 && (
+        <div className="">
+          <DataUmum />
+        </div>
+      )}
       {tapIndex == 1 && <div>Penetapan</div>}
       {tapIndex == 2 && <div>Jadwal Sidang</div>}
       {tapIndex == 3 && <div>Saksi</div>}
