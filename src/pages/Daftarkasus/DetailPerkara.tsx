1ca44954cb6c14a529c053c1839f47626b61e084
Simon Tampubolon <smntampubolon18@gmail.com>
2024-04-01 15:02:55 +0700

    Add button detail perkara

--- src/pages/Daftarkasus/DetailPerkara.tsx
+++ src/pages/Daftarkasus/DetailPerkara.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import PenetapanPerkara from './PenetapanPerkara';
-import DataUmum from '../../components/DataUmum';
+import DataUmum from './DataUmum';
 
 const DetailPerkara = () => {
   const [tapIndex, setTapIndex] = useState(0);
@@ -9,30 +9,45 @@ const DetailPerkara = () => {
     'Penetapan',
     'Jadwal Sidang',
     'Saksi',
+    'Penuntut',
+    'Putusan',
+    'Banding',
     'Barang Bukti',
     'Riwayat Perkara',
   ];
   return (
     <div>
-      <div className="flex flex-row pt-5">
-        {tabMenu.map((data, index) => {
-          return (
-            <button className="bg-white" onClick={() => setTapIndex(index)}>
-              {data}
-            </button>
-          );
-        })}
-      </div>
-      {tapIndex == 0 && (
-        <div className="">
-          <DataUmum />
+      <div className=" p-2 bg-slate-500 m-3">
+        <div className="flex flex-row mt-3">
+          {tabMenu.map((data, index) => {
+            return (
+              <button
+                className="bg-slate-400 text-white hover:bg-green-500 text-black-700 font-semibold hover:text-white border-black-500 hover:border-transparent rounded p-2 ml-3"
+                onClick={() => setTapIndex(index)}
+              >
+                {data}
+              </button>
+            );
+          })}
         </div>
-      )}
-      {tapIndex == 1 && <div>Penetapan</div>}
-      {tapIndex == 2 && <div>Jadwal Sidang</div>}
-      {tapIndex == 3 && <div>Saksi</div>}
-      {tapIndex == 4 && <div>Barang Bukti</div>}
-      {tapIndex == 5 && <div>Riwayat Perkara</div>}
+
+        <div className="bg-slate-700">
+          {tapIndex == 0 && (
+            <div className="">
+              <DataUmum />
+            </div>
+          )}
+          {tapIndex == 1 && <div>Penetapan</div>}
+          {tapIndex == 2 && <div>Jadwal Sidang</div>}
+          {tapIndex == 3 && <div>Saksi</div>}
+          {tapIndex == 4 && <div>Penuntut</div>}
+          {tapIndex == 5 && <div>Putusan</div>}
+          {tapIndex == 6 && <div>Banding</div>}
+          {tapIndex == 7 && <div>Barang Bukti</div>}
+          {tapIndex == 8 && <div>Riwayat Perkara</div>}
+        </div>
+      </div>
+
       {/* <h1>Hello World!</h1>
       <h2>Halaman Detail</h2> */}
     </div>
