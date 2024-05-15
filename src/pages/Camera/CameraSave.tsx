aa61f59263ba61c2e1544d04ff61122726829b08
DandanIsmail42 <105099782+DandanIsmail42@users.noreply.github.com>
2024-05-15 18:04:59 +0700

    wip(camera save): Update layout

--- src/pages/Camera/CameraSave.tsx
+++ src/pages/Camera/CameraSave.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import GambarKamera from '../../images/camera_images_test.jpeg';
 import { IoAdd } from 'react-icons/io5';
 import { apiBuilding } from '../../services/api';
+import { ModalAddCameraSave } from './ModalAddCameraSave';
 
 const CameraSave = () => {
   const [addCamera, setAddCamera] = useState(false);
@@ -9,6 +10,7 @@ const CameraSave = () => {
   const [selectedRoom, setSelectedRoom] = useState('');
   const [selectedBuilding, setSelectedBuilding] = useState('');
   const [selectedFloor, setSelectedFloor] = useState('');
+  const [modalAddOpen, setModalAddOpen] = useState(false);
   const [selectedCamera, setSelectedCamera] = useState('');
   const [currentPage, setCurrentPage] = useState(1);
   const [pilihKamera, setPilihKamera] = useState('');
@@ -23,6 +25,9 @@ const CameraSave = () => {
     console.log('Add Camera');
   };
 
+  const handleCloseAddModal = () => {
+    setModalAddOpen(false);
+  };
   const fetchData = async () => {
     try {
       let dataLocal = localStorage.getItem('dataUser');
@@ -78,198 +83,35 @@ const CameraSave = () => {
 
   return (
     <>
-      {!addCamera ? (
-        <>
-          <div className=" flex flex-row gap-6 py-6 px-10">
-            {DataKamera.map((item, index) => (
-              <div key={index}>
-                <div className="bg-slate-500 py-10 px-16 rounded-2xl text-2xl text-white">
-                  <p>{item.name}</p>
-                </div>
-              </div>
-            ))}
-            <div className="text-white" onClick={handleAddCamera}>
-              <div className="bg-meta-4-dark rounded-2xl py-2 px-2">
-                <div className="bg-slate-600 py-6 px-18 rounded-2xl">
-                  <IoAdd size={51} />
-                </div>
-              </div>
+      <button
+        onClick={() => setModalAddOpen(true)}
+        className="  text-black rounded-md font-semibold bg-blue-300 py-2 px-3 b-tambah"
+      >
+        Tambah
+      </button>
+      <div className=" flex flex-row gap-6 py-6 px-10">
+        {DataKamera.map((item, index) => (
+          <div key={index}>
+            <div className="bg-slate-500 py-10 px-16 rounded-2xl text-2xl text-white">
+              <p>{item.name}</p>
             </div>
           </div>
-        </>
-      ) : (
-        <>
-          <form action="">
-            <div className=" flex py-6 flex-col items-center gap-5">
-              <div className="flex flex-row gap-3 bg-slate-500 w-3/4 text-xl justify-start px-6 py-3 rounded-xl">
-                <p className=" text-white">Nama Tampilan :</p>
-                <input
-                  type="text"
-                  className="border-none focus:outline-none bg-transparent text-white w-[74%]"
-                />
-              </div>
-              <div className="flex flex-row gap-6 w-3/4 h-[60vh] text-xl justify-start py-3 rounded-xl">
-                <div className="h-full w-1/2 bg-slate-500 rounded-2xl flex flex-col items-center pt-10">
-                  <div className="flex gap-10 flex-col">
-                    <select
-                      value={selectedBuilding}
-                      onChange={handleSelectBuilding}
-                      className="p-2 border rounded bg-meta-4 font-semibold w-full"
-                    >
-                      <option disabled value="">
-                        Pilih Gedung
-                      </option>
-                      {buildings?.data?.records?.gedung?.map((building) => (
-                        <option
-                          key={building.gedung_otmil_id}
-                          value={building.gedung_otmil_id}
-                        >
-                          {building.nama_gedung_otmil}
-                        </option>
-                      ))}
-                    </select>
-
-                    {selectedBuilding && (
-                      <>
-                        {buildings?.data?.records?.gedung?.find(
-                          (building) =>
-                            building.gedung_otmil_id === selectedBuilding,
-                        )?.lantai.length > 0 && (
-                          <select
-                            value={selectedFloor}
-                            onChange={handleSelectFloor}
-                            className="p-2 border rounded bg-meta-4 font-semibold w-full"
-                          >
-                            <option disabled value="">
-                              Pilih Lantai
-                            </option>
-                            {buildings?.data?.records?.gedung
-                              ?.find(
-                                (building) =>
-                                  building.gedung_otmil_id === selectedBuilding,
-                              )
-                              ?.lantai.map((floor) => (
-                                <option
-                                  key={floor.lantai_otmil_id}
-                                  value={floor.lantai_otmil_id}
-                                >
-                                  {floor.nama_lantai}
-                                </option>
-                              ))}
-                          </select>
-                        )}
-                      </>
-                    )}
-                  </div>
-                </div>
-
-                <div className="h-full w-1/2 bg-slate-500 rounded-2xl flex flex-col items-center pt-10">
-                  <div className="flex gap-10 flex-col">
-                    {selectedFloor && (
-                      <>
-                        {buildings?.data?.records?.gedung
-                          ?.find(
-                            (building) =>
-                              building.gedung_otmil_id === selectedBuilding,
-                          )
-                          ?.lantai.find(
-                            (floor) => floor.lantai_otmil_id === selectedFloor,
-                          )?.ruangan.length > 0 && (
-                          <select
-                            value={selectedRoom}
-                            onChange={(e) => setSelectedRoom(e.target.value)}
-                            className="p-2 border rounded bg-meta-4 font-semibold w-full"
-                          >
-                            <option disabled value="">
-                              Pilih Ruangan
-                            </option>
-                            {buildings?.data?.records?.gedung
-                              ?.find(
-                                (building) =>
-                                  building.gedung_otmil_id === selectedBuilding,
-                              )
-                              ?.lantai.find(
-                                (floor) =>
-                                  floor.lantai_otmil_id === selectedFloor,
-                              )
-                              ?.ruangan.map((room) => (
-                                <option
-                                  key={room.ruangan_otmil_id}
-                                  value={room.ruangan_otmil_id}
-                                  onClick={() =>
-                                    handleClickRoom(room.ruangan_otmil_id)
-                                  }
-                                >
-                                  {room.nama_ruangan_otmil}
-                                </option>
-                              ))}
-                          </select>
-                        )}
-                      </>
-                    )}
-                    {selectedRoom && (
-                      <>
-                        {buildings?.data?.records?.gedung
-                          ?.find(
-                            (building) =>
-                              building.gedung_otmil_id === selectedBuilding,
-                          )
-                          ?.lantai.find(
-                            (floor) => floor.lantai_otmil_id === selectedFloor,
-                          )
-                          ?.ruangan.find(
-                            (room) => room.ruangan_otmil_id === selectedRoom,
-                          )?.kamera.length > 0 && (
-                          <select
-                            value={pilihKamera}
-                            onChange={(e) => {
-                              handleClickKamera(e.target.value);
-                              setPilihKamera(e.target.value);
-                              // console.log('eTargetValue', e.target.value);
-                            }}
-                            className="p-2 border rounded bg-meta-4 font-semibold w-full"
-                          >
-                            <option disabled value="">
-                              Pilih Kamera
-                            </option>
-                            {buildings?.data?.records?.gedung
-                              ?.find(
-                                (building) =>
-                                  building.gedung_otmil_id === selectedBuilding,
-                              )
-                              ?.lantai.find(
-                                (floor) =>
-                                  floor.lantai_otmil_id === selectedFloor,
-                              )
-                              ?.ruangan.find(
-                                (room) =>
-                                  room.ruangan_otmil_id === selectedRoom,
-                              )
-                              ?.kamera.map((cam) => (
-                                <option
-                                  key={cam.kamera_id}
-                                  value={JSON.stringify(cam)}
-                                  onClick={() => handleClickKamera(cam)}
-                                >
-                                  {cam.nama_kamera}
-                                </option>
-                              ))}
-                          </select>
-                        )}
-                      </>
-                    )}
-                  </div>
-                </div>
-              </div>
-
-              <div className="flex flex-row gap-3 w-3/4 text-xl rounded-xl justify-end mt-[-1%]">
-                <div className="bg-green-500 py-2 px-8 rounded-lg">
-                  <p className="text-black text-[1rem]">Save</p>
-                </div>
-              </div>
+        ))}
+        <div className="text-white" onClick={handleAddCamera}>
+          <div className="bg-meta-4-dark rounded-2xl py-2 px-2">
+            <div className="bg-slate-600 py-6 px-18 rounded-2xl">
+              <IoAdd size={51} />
             </div>
-          </form>
-        </>
+          </div>
+        </div>
+      </div>
+
+      {modalAddOpen && (
+        <ModalAddCameraSave
+          closeModal={handleCloseAddModal}
+          // onSubmit={handleInsertGedungOtmil}
+          // token={token}
+        />
       )}
     </>
   );
