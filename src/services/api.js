a6249265fcb5af3dde6de9b4c4a6b00fb25baa71
sslthn31 <alhambra.sulthan@gmail.com>
2024-06-14 15:54:44 +0700

    wiring barang bukti

--- src/services/api.js
+++ src/services/api.js
@@ -567,7 +567,7 @@ export async function apiUpdateBarangBukti(params, token) {
   try {
     const response = await axios({
       method: 'POST',
-      url: `${newBaseUrl}/barang_bukti_kasus`,
+      url: `${newBaseUrl}/barang_bukti_kasus?_method=PUT`,
       data: params,
       headers: {
         'Content-Type': 'multipart/form-data',
@@ -585,8 +585,8 @@ export async function apiUpdateBarangBukti(params, token) {
 export async function apiDeleteBarangBukti(params, token) {
   try {
     const response = await axios({
-      method: 'post',
-      url: newwebserviceurl + 'siram_api/barang_bukti_kasus_delete.php',
+      method: 'DELETE',
+      url: `${newBaseUrl}/barang_bukti_kasus`,
       data: params,
       headers: {
         'Content-Type': 'application/json',
